d38f50f4e45a59430dc112a24c8daeb41d8e6329
skinnybuff <s_t_white@hotmail.com>
2013-10-23 22:17:48 -0400

    hurtl tut init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = '48f3c6d925f1f572540900f6dabb2404425d53771dcbc068f3c60847f76cb65a28c67503ff8d7534f4914295f40fb228cc77721f69f744a044b2908ceb7dd76b'
